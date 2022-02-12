e5a13b377eba5c4f07cd6b79329c90606725a2c6
pcancio <pcancio@gmail.com>
2022-02-11 22:26:04 -0500

    save history function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,18 @@
 var apiKey = "24ea2481120915a964d11bfbdcc72405";
 
+var searchWeather = document.getElementById('searchBtn');
+var cityCol = document.getElementById('city-col');
 var cityInput = document.getElementById('city');
-// GET CURRENT WEATHER
+var currentWeather = document.getElementById('today-temp');
+var fiveDayForecast = document.getElementById('five-day-forecast');
+var cityHistory = JSON.parse(localStorage.getItem('cityHistory')) || [];
+var btnContainer = document.getElementById('button-container');
 
-var searchCity = function(event, city) {
-    event.preventDefault();
-    var apiURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-    fetch(apiURL)
-        .then(function(response) {
-            if (response.ok) {
-                response.json()
-                    .then(function(data) {
-                        saveHistory(city);
-                        displayWeather(data.coord.lon, data.coord.lat);
-                        $('city').val('');
-                    })
-            } else {
-                alert('Error:' + response.statusText);
-            }
-        })
-        .catch(function(error) {
-            alert('Unable to connect to API', error);
-        })
-};
-
-var displayWeather = function(long, lat) {
-    var apiURL = "api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + long + "&appid=" + apiKey;
+// function to save to local storage and display previous searches on dashboard
+var saveHistory = function(city) {
+    if (cityHistory.indexOf(city) === -1) {
+        cityHistory.push(city);
+        localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
+    }
 
 }
-
-
-
-// var getCurrentWeather = function(city) {
-//     cityInput.value="";
-//     // get today's date
-//     var today = new Date();
-//     var dd = String(today.getDate()).padStart(2,'0');
-//     var mm = String(today.getMonth() + 1).padStart(2, '0');
-//     var yyyy = today.getFullYear();
-//     today = mm + '/' + dd + '/' + yyyy;
-
-
-// }
