31c83e4bf5118baf675e70f99d82e76b2dee607d
pcancio <pcancio@gmail.com>
2022-02-11 22:46:36 -0500

    apis linked

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,45 @@ var fiveDayForecast = document.getElementById('five-day-forecast');
 var cityHistory = JSON.parse(localStorage.getItem('cityHistory')) || [];
 var btnContainer = document.getElementById('button-container');
 
+var lat;
+var long;
+
 // function to save to local storage and display previous searches on dashboard
 var saveHistory = function(city) {
     if (cityHistory.indexOf(city) === -1) {
         cityHistory.push(city);
         localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
     }
+};
 
-}
+var getCurrentWeather = function(city) {
+    cityInput.value = '';
+    // get today's date
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+
+};
+
+// call to get the city name from the api
+var apiCity = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+fetch(apiCity).then(function(response) {
+    if (response.ok) {
+        response.json().then(function(data) {
+            lat = data.results[0].locations[0].displayLatLng.lat;
+            long = data.results[0].locations[0].displayLatLng.long;
+        })
+    }
+});
+
+//    call to get the long and lat from the api 
+var apiLongLat = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + long + "&appid=" + apiKey;
+fetch(apiLongLat).then(function(response) {
+    if (response.ok) {
+        response.json().then(function(data) {
+            currentWeather.innerHTML = "";
+        })
+    }
+});
